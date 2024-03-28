46a25defb04bcb7406c3432ebd407bcf6c7dc308
Ricardo Alcantara <ricardo.alcantara@linkupconsultoria.com.br>
2024-03-27 23:02:14 -0300

    primeiro commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["apptest/apptest/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"apptest/apptest/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
